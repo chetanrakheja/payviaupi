f75c856d759c4cbe75d9b2819a66a433a0ae37cd
Chetan Rakheja <chetan.rakheja@orange.com>
2024-09-10 15:09:03 +0530

    made footer mobile optimzed as well

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,21 +3,32 @@ import Link from 'next/link';
 const currentYear = new Date().getFullYear();
 
 export function Footer() {
-    // const session = useSession();
-    <div className=""></div>
-    return <footer className="flex justify-between px-20 pt-4 border-t-2 border-black dark:border-white p-4">
-    <div className="flex column responsive">
-    Created by <a href="https://www.linkedin.com/in/chetanrakheja/" target="_blank">&nbsp;Chetan Rakheja</a>
-    </div>
-    <div>
-      <a
-        href="https://github.com/chetanrakheja/payviaupi"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Visit Github Repo
-      </a>
-    </div>
-    <div className="flex center">© Copyright {currentYear}</div>
-  </footer>
+  return (
+    <footer className="flex flex-col md:flex-row justify-between items-center px-4 md:px-20 py-4 border-t-2 border-black dark:border-white">
+      <div className="flex flex-col md:flex-row items-center mb-4 md:mb-0">
+        Created by 
+        <a
+          href="https://www.linkedin.com/in/chetanrakheja/"
+          target="_blank"
+          className="ml-1 text-blue-500"
+          rel="noopener noreferrer"
+        >
+          Chetan Rakheja
+        </a>
+      </div>
+      <div className="mb-4 md:mb-0">
+        <a
+          href="https://github.com/chetanrakheja/payviaupi"
+          target="_blank"
+          className="text-blue-500"
+          rel="noopener noreferrer"
+        >
+          Visit Github Repo
+        </a>
+      </div>
+      <div className="text-center">
+        © {currentYear}
+      </div>
+    </footer>
+  );
 }
